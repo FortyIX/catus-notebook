366d19982bd1628f16f89107373b38a4f89a58a7
Fu Zhang <zhangfu2014@live.com>
2021-09-11 19:45:59 +0800

    added reserved file for calander and main page. added the side nav bar

---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mainWindow">
+      <h1> This is the main page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+
+
+
+
+export default class Main extends Vue {
+  msg!: string
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.mainWindow{
+    height: 700px;
+    width: 100%;
+    background: green;
+}
+</style>
