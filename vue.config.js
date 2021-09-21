2b49b1c135c49179592a50f7597e60acebb4a32f
Fu Zhang <zhangfu2014@live.com>
2021-09-21 21:45:40 +0800

    updated logo and added build configurations

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,40 @@
 module.exports = {
     pluginOptions: {
         electronBuilder: {
-          nodeIntegration: true,
-          }
-     }
+            nodeIntegration: true,
+            builderOptions:{
+                productName:'Catus Notebook',
+                appId: 'com.juizeff.catus-noteook',
+                win: {
+                  icon: 'public/icon/logo256.ico'
+                },
+                linux:{
+                  icon: 'public/icon/logo267.ico'  
+                },
+                nsis:{
+                    "oneClick":false,
+                    "allowToChangeInstallationDirectory": true
+                },
+                files:[
+                    "**/*", 
+                ],
+                extraResources:[
+                    "stage/*",
+                    "documents/*",
+                ],
+                extraFiles:[
+                    {
+                        "from": "./license/",
+                        "to": "resources/license",
+                        "filter":[
+                            "**/*"
+                        ]
+                    },
+                ]
+
+                
+            }
+        }
+    }
+    //...
 }
